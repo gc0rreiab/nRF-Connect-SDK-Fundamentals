2b7615eb74811c28366b849feb1e7a914df3d951
gcb <gabriel.brito.95@gmail.com>
2024-01-27 10:02:17 +0000

    Add Kconfig file. Change CMakeList.txt to make sure the addition of the custom files to be conditional. Use proj.conf file to enable configurations

---
+++ fund_less3_exer2/src/main.c
@@ -0,0 +1,18 @@
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#ifdef CONFIG_MYFUNCTION
+#include "myfunction.h"
+#endif
+
+int main(void)
+{
+	while(1){
+		#ifdef CONFIG_MYFUNCTION
+		int a = 3, b = 4;
+		printk("The sum of %d and %d is %d\n\r", a, b, sum(a,b));
+		#else
+		printk("MYFUNCTION not enabled\r\n");
+		#endif
+		k_msleep(1000);
+	}
+}
