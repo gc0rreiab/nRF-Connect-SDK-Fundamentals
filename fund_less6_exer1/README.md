1e9a31926fa276541d9ec110cefa11d929bcd568
gcb <gabriel.brito.95@gmail.com>
2024-02-16 23:19:45 +0000

    Update README file explaining that exercise 1 uses SHT25 sensor and exercise 2 uses bno055 imu sensor

--- fund_less6_exer1/README.md
+++ fund_less6_exer1/README.md
@@ -1,8 +1,13 @@
 # FUND_LESS6_EXER1: 
 
 ## Hardware/Sensors used
-These lessons about **I2C peripheral drivers** were done using different sensors than described in the the course. I've used I2C sensors I have with me during the time I've performing this course:
-- **Bosch BNO055 IMU sensor:**
+These lessons about **I2C peripheral drivers** were done using different sensors than described in the the course. I've used I2C sensors I have with me during the time I've performing this course.
+
+- **Exercise 1: Sensirion SHT25 sensor**
+ 
+   I've implemented a raw communication based on the [sensor datasheet](https://www.sensirion.com/products/catalog/SHT25/). Several MACROS with sensor register addresses were defined and then using Zephyr I2C API functions such as i2c_write_dt() and i2c_write_read_dt() the I2C communication was established with the sensor.
+
+- **Exercise 2: Bosch BNO055 IMU sensor:**
  
     I've integrated the official and open-source [BNO055 C driver](https://github.com/boschsensortec/BNO055_driver/tree/master) developed by Bosch. The bno055_support.c file was used as reference to integrate the sensor C driver with ncs I2C API. 
 
@@ -18,7 +23,7 @@ Besided the following instructions:
     project(i2c_exercise1)
     target_sources(app PRIVATE src/main.c)
 
-Also add the following instruction to make sure to add the sensor C driver to the build process if the variable **CONFIG_BNO055_SENSOR** is set in Kconfig file:
+On exercise 2, also add the following instruction to make sure to add the sensor C driver to the build process if the variable **CONFIG_BNO055_SENSOR** is set in Kconfig file:
 
     target_sources_ifdef(CONFIG_BNO055_SENSOR app PRIVATE src/bno055.c)
 
--- fund_less6_exer1/src/main.c
+++ fund_less6_exer1/src/main.c
@@ -4,7 +4,54 @@
 #include <zephyr/sys/printk.h>
 #include <zephyr/drivers/i2c.h>
 
-void main(void){
+//Sensor registers addresses
+#define SHT25_ADDR          0x40
+#define SHT25_TRIGGER_TEMP  0xF3    // no-hold master mode
+#define SHT25_TRIGGER_RH    0xF5    // no-hold master mode
+#define SHT25_USER_REG_W    0xE6
+#define SHT25_USER_REG_R    0xE7
+#define SHT25_SOFT_RST      0xFE
+
+// Get node identifier symbol I2C0_NODE based on bn055 sensor label on device-tree overlay file
+#define I2C0_NODE DT_NODELABEL(sht25)
+
+// Get the device pointer for the I2C0 bus
+static const struct i2c_dt_spec dev_i2c0 = I2C_DT_SPEC_GET(I2C0_NODE);
+
+int main(void){
+
+    uint8_t ret;
+
+    // Check if the I2C0 peripheral is ready 
+    if (!device_is_ready(dev_i2c0.bus)) {
+	    printk("I2C bus %s is not ready!\n\r", dev_i2c0.bus->name);
+	    return -1;
+    }
+
+    // Configure the SHT25 sensor using the user register
+    // Measurements resolutions configured: Temp: 12 bits and HR: 8 bits
+    uint8_t config[2] = {SHT25_USER_REG_R, 0x8C};
+	ret = i2c_write_dt(&dev_i2c0, config, sizeof(config));
+	if(ret != 0){
+		printk("Failed to write to I2C device address %x at Reg. %x \n", dev_i2c0.addr,config[0]);
+		return -1;
+	}
+
+    uint8_t temp_reading[2]= {0};
+    uint8_t sensor_regs[2] ={STTS751_TEMP_LOW_REG,STTS751_TEMP_HIGH_REG};
+    ret = i2c_write_read_dt(&dev_i2c,&sensor_regs[0],1,&temp_reading[0],1);
+    if(ret != 0){
+	    printk("Failed to write/read I2C device address %x at Reg. %x \r\n", dev_i2c.addr,sensor_regs[0]);
+    }
+    ret = i2c_write_read_dt(&dev_i2c,&sensor_regs[1],1,&temp_reading[1],1);
+    if(ret != 0){
+	    printk("Failed to write/read I2C device address %x at Reg. %x \r\n", dev_i2c.addr,sensor_regs[1]);
+    }
+
+    while(1){
+
+
+    }
 
 
 
