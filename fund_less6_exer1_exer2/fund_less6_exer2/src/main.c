881d2544ee54eb8b7d92c21cbddee0a79a5f9380
gcb <gabriel.brito.95@gmail.com>
2024-02-19 17:22:57 +0000

    Integrated BNO055_driver with Zephyr I2C API functions

--- fund_less6_exer1_exer2/fund_less6_exer2/src/main.c
+++ fund_less6_exer1_exer2/fund_less6_exer2/src/main.c
@@ -3,17 +3,26 @@
 #include <zephyr/devicetree.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/drivers/i2c.h>
+#include "bno055.h" // Include bno055 header file
 
-// Get node identifier symbol I2C0_NODE based on bn055 sensor label on device-tree overlay file
-#define I2C0_NODE DT_NODELABEL(bno055)
+// Function prototypes to integrate Zephyr I2C functions into the BNO055 driver API
+int8_t BNO055_I2C_bus_write(uint8_t dev_addr, uint8_t reg_addr, uint8_t *reg_data, uint8_t cnt);
+int8_t BNO055_I2C_bus_read(uint8_t dev_addr, uint8_t reg_addr, uint8_t *reg_data, uint8_t cnt);
+void BNO055_delay_msek(uint32_t msek);
+int8_t I2C_routine(void);
+
+// Declaration of bno055 object
+struct bno055_t bno055;
+
+// Define a macro to fetch the node identifier symbol for I2C0 based on its label
+#define I2C0_NODE DT_NODELABEL(sht25)
 
 // Get the device pointer for the I2C0 bus
 static const struct i2c_dt_spec dev_i2c0 = I2C_DT_SPEC_GET(I2C0_NODE);
 
 int main(void)
 {
-
-    uint8_t ret = 0;
+    uint8_t err = 0;
 
     // Check if the I2C0 peripheral is ready
     if (!device_is_ready(dev_i2c0.bus))
@@ -22,10 +31,52 @@ int main(void)
         return -1;
     }
 
+    err = I2C_routine();
+    err = bno055_init(&bno055);
+
     while (1)
     {
 
         // Delay the loop to perform one temperature reading each second
         k_sleep(K_MSEC(1000));
     }
+}
+
+// Integrate BNO055 API with Zephyr I2C write() and read() functions
+int8_t I2C_routine(void)
+{
+    bno055.bus_write = BNO055_I2C_bus_write;
+    bno055.bus_read = BNO055_I2C_bus_read;
+    bno055.delay_msec = BNO055_delay_msek;
+    bno055.dev_addr = BNO055_I2C_ADDR1;
+
+    return BNO055_INIT_VALUE;
+}
+
+int8_t BNO055_I2C_bus_write(uint8_t dev_addr, uint8_t reg_addr, uint8_t *reg_data, uint8_t cnt)
+{
+    int8_t BNO055_iERROR = BNO055_INIT_VALUE;
+    uint8_t config[cnt + 1];
+    config[0] = reg_addr;
+    bytecpy(config + 1, reg_data, cnt);
+
+    BNO055_iERROR = i2c_write(dev_i2c0.bus, config, cnt, dev_addr);
+
+    return BNO055_iERROR;
+}
+
+int8_t BNO055_I2C_bus_read(uint8_t dev_addr, uint8_t reg_addr, uint8_t *reg_data, uint8_t cnt)
+{
+
+    int8_t BNO055_iERROR = BNO055_INIT_VALUE;
+
+    BNO055_iERROR = i2c_write_read(dev_i2c0.bus, dev_addr, &reg_addr, 1, reg_data, cnt);
+
+    return BNO055_iERROR;
+}
+
+void BNO055_delay_msek(uint32_t msek)
+{
+    // Delay
+    k_sleep(K_MSEC(msek));
 }
